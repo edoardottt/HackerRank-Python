fdea91372ebf941e2ce9bbb3ae7471051a268262
edoardottt <edoardott@gmail.com>
2020-06-19 16:15:53 +0200

    update

---
+++ 0032.Set.intersection()Operation/solution.py
@@ -0,0 +1,6 @@
+n1 = int(input())
+s1 = set(input().split())
+n2 = int(input())
+s2 = set(input().split())
+
+print(len(s1.intersection(s2)))
---
+++ 0034.Set.symmetric_difference()Operation/solution.py
@@ -0,0 +1,5 @@
+n1 = int(input())
+s1 = set(input().split())
+n2 = int(input())
+s2 = set(input().split())
+print(len(s1.symmetric_difference(s2)))
